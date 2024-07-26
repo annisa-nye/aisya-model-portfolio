a4c21b967ca3209c83a9aafaa58528267fab870b
annisa-nye <annisanye99@gmail.com>
2024-07-26 17:35:30 +1000

    added paralax effect

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HeroSection: React.FC = () => (
-	<section className="w-full h-screen p-10 border-slate-950 border-2 bg-[url('./assets/images/p1-red-yellow/p1-3.jpg')] bg-cover">
+	<section className="w-full h-screen p-10 border-slate-950 border-2 bg-[url('./assets/images/p1-red-yellow/p1-3.jpg')] bg-cover bg-fixed">
 		<div className='py-20 text-start text-blue-300'>
 			<h1 className='text-7xl'>Aisya Wiley</h1>
 			<p className='text-2xl'>Model</p>
