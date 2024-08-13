90fec71f72c5ed91c505468347499b26237e9f45
annisa-nye <annisanye99@gmail.com>
2024-08-13 12:48:50 +1000

    contact form responsive

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,47 +3,46 @@ import { Phone, Mail, MapPin, Send } from "lucide-react";
 
 const Contact: React.FC = () => {
   return (
-    <section
-      id="contact"
-      className="min-h-screen w-full bg-gray-50 p-6 font-inter"
-    >
-      <div className="mx-auto max-w-5xl">
-        <h2 className="mb-4 text-4xl font-bold xs:text-3xl sm:mb-2">
-          Contact Me
-        </h2>
-        <p className="mb-8 text-xl text-gray-600 xs:text-lg sm:mb-4">
-          How to get in touch
-        </p>
-
+    <section id="contact" className="min-h-screen w-full font-inter">
+      <h2 className="bg-white px-6 py-6 text-4xl font-bold text-black xs:text-xl sm:ml-4 sm:pb-2 sm:pl-2 sm:pt-4 sm:text-2xl">
+        Contact
+      </h2>
+      <div className="mx-6 max-w-7xl">
         <div className="flex flex-row gap-8 sm:flex-row sm:items-start sm:justify-center sm:gap-12">
           {/* Contact Information */}
           <div className="w-full space-y-8 sm:w-2/5">
             <div className="flex items-center">
-              <Phone className="mr-4 h-8 w-8 text-blue-500" />
+              <Phone className="mr-4 h-8 w-8 text-red-500" />
               <div>
-                <h3 className="text-xl font-semibold xs:text-lg">Mobile</h3>
-                <span className="text-gray-600 xs:text-sm">
+                <h3 className="text-xl font-semibold xs:text-lg sm:text-lg">
+                  Mobile
+                </h3>
+                <span className="text-gray-600 xs:text-sm sm:text-sm">
                   +61 424 833 056
                 </span>
               </div>
             </div>
             <div className="flex items-center">
-              <Mail className="mr-4 h-8 w-8 text-blue-500" />
+              <Mail className="mr-4 h-8 w-8 text-red-500" />
               <div>
-                <h3 className="text-xl font-semibold xs:text-lg">Email</h3>
+                <h3 className="text-xl font-semibold xs:text-lg sm:text-lg">
+                  Email
+                </h3>
                 <a
                   href="mailto:annisanye@outlook.com"
-                  className="text-gray-600 transition-colors hover:text-blue-500 xs:text-sm"
+                  className="text-gray-600 transition-colors hover:text-red-500 xs:text-sm sm:text-sm"
                 >
                   annisanye@outlook.com
                 </a>
               </div>
             </div>
             <div className="flex items-center">
-              <MapPin className="mr-4 h-8 w-8 text-blue-500" />
+              <MapPin className="mr-4 h-8 w-8 text-red-500" />
               <div>
-                <h3 className="text-xl font-semibold xs:text-lg">Location</h3>
-                <span className="text-gray-600 xs:text-sm">
+                <h3 className="text-xl font-semibold xs:text-lg sm:text-lg">
+                  Location
+                </h3>
+                <span className="text-gray-600 xs:text-sm sm:text-sm">
                   Melbourne, Victoria
                 </span>
               </div>
@@ -57,7 +56,7 @@ const Contact: React.FC = () => {
                 <div>
                   <label
                     htmlFor="fullName"
-                    className="mb-1 block text-sm font-medium text-gray-700"
+                    className="mb-1 block text-xl font-medium text-gray-700 sm:text-lg"
                   >
                     Full Name
                   </label>
@@ -66,14 +65,14 @@ const Contact: React.FC = () => {
                     name="fullName"
                     id="fullName"
                     placeholder="Enter full name"
-                    className="w-full rounded-md border border-gray-300 bg-blue-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="w-full rounded-md border border-gray-300 bg-red-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 sm:text-md"
                     required
                   />
                 </div>
                 <div>
                   <label
                     htmlFor="email"
-                    className="mb-1 block text-sm font-medium text-gray-700"
+                    className="mb-1 block text-xl font-medium text-gray-700 sm:text-lg"
                   >
                     Email
                   </label>
@@ -81,8 +80,8 @@ const Contact: React.FC = () => {
                     type="email"
                     name="email"
                     id="email"
-                    placeholder="Enter email address"
-                    className="w-full rounded-md border border-gray-300 bg-blue-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Enter email"
+                    className="w-full rounded-md border border-gray-300 bg-red-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 sm:text-md"
                     required
                   />
                 </div>
@@ -90,7 +89,7 @@ const Contact: React.FC = () => {
               <div>
                 <label
                   htmlFor="subject"
-                  className="mb-1 block text-sm font-medium text-gray-700"
+                  className="mb-1 block text-xl font-medium text-gray-700 sm:text-lg"
                 >
                   Subject
                 </label>
@@ -99,14 +98,14 @@ const Contact: React.FC = () => {
                   name="subject"
                   id="subject"
                   placeholder="Enter a subject line"
-                  className="w-full rounded-md border border-gray-300 bg-blue-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full rounded-md border border-gray-300 bg-red-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 sm:text-md"
                   required
                 />
               </div>
               <div>
                 <label
                   htmlFor="message"
-                  className="mb-1 block text-sm font-medium text-gray-700"
+                  className="mb-1 block text-xl font-medium text-gray-700 sm:text-lg"
                 >
                   Message
                 </label>
@@ -115,14 +114,14 @@ const Contact: React.FC = () => {
                   id="message"
                   rows={6}
                   placeholder="Enter your message"
-                  className="w-full rounded-md border border-gray-300 bg-blue-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full rounded-md border border-gray-300 bg-red-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 sm:text-md"
                   required
                 ></textarea>
               </div>
               <div>
                 <button
                   type="submit"
-                  className="flex items-center justify-center rounded-md bg-blue-500 px-6 py-3 text-white transition-colors hover:bg-blue-600"
+                  className="flex items-center justify-center rounded-md bg-red-500 px-6 py-3 text-white transition-colors hover:bg-red-700"
                 >
                   Send Message
                   <Send className="ml-2 h-5 w-5" />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer: React.FC = () => (
-	<footer className='w-full p-10 border-slate-950 border-2'>
+	<footer className='w-full bg-red-500 py-10'>
 		<p>&copy; 2024 Aisya. All rights reserved.</p>
 		<div className='social-links'>{/* Add social media links here */}</div>
 		<nav>{/* Add additional menu items here */}</nav>
